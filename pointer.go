1a6fe3a61972e0a45c547d5a30e8f091984c0922
Arnaud Delobelle <arnodel@gmail.com>
2021-02-26 20:54:14 +0000

    Fix dynamic scaling, add debugging for windows

---
+++ pointer.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"image"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type PointerStatus int
+
+const (
+	NoTouch PointerStatus = iota
+	TouchDown
+	Dragging
+	TouchUp
+)
+
+type PointerTracker struct {
+	startPos    image.Point
+	lastPos     image.Point
+	currentPos  image.Point
+	status      PointerStatus
+	frames      int
+	cancelTouch bool
+}
+
+func (p *PointerTracker) CancelTouch() {
+	p.cancelTouch = true
+	p.status = NoTouch
+}
+
+func (p *PointerTracker) Update() {
+	var currentPos image.Point
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		currentPos = image.Pt(ebiten.CursorPosition())
+	} else if touchIDs := ebiten.TouchIDs(); touchIDs != nil {
+		currentPos = image.Pt(ebiten.TouchPosition(touchIDs[0]))
+	} else {
+		p.cancelTouch = false
+		switch p.status {
+		case NoTouch:
+			// Nothing to do?
+		case TouchDown, Dragging:
+			p.status = TouchUp
+			p.lastPos = p.currentPos
+			p.frames = 0
+		case TouchUp:
+			p.status = NoTouch
+		}
+		return
+	}
+	if p.cancelTouch {
+		return
+	}
+	switch p.status {
+	case NoTouch, TouchUp:
+		p.status = TouchDown
+		p.startPos = currentPos
+		p.lastPos = currentPos
+		p.frames = 1
+	case TouchDown:
+		p.status = Dragging
+		fallthrough
+	case Dragging:
+		if currentPos == p.lastPos && p.frames > 0 {
+			p.frames++
+		} else {
+			p.lastPos = p.currentPos
+			p.frames = 0
+		}
+	default:
+		// Shouldn't get there?
+	}
+	p.currentPos = currentPos
+	return
+}
--- window.go
+++ window.go
@@ -2,50 +2,78 @@ package main
 
 import (
 	"image"
+	"image/color"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+func drawRect(dst *ebiten.Image, r image.Rectangle, c color.Color) {
+	x1, y1 := fc(r.Min)
+	x2, y2 := fc(r.Max)
+	ebitenutil.DrawLine(dst, x1, y1, x1, y2, c)
+	ebitenutil.DrawLine(dst, x1, y1, x2, y1, c)
+	ebitenutil.DrawLine(dst, x2, y1, x2, y2, c)
+	ebitenutil.DrawLine(dst, x2, y2, x1, y2, c)
+}
+
 type Window struct {
-	bounds image.Rectangle
-	tr     ebiten.GeoM
+	outerBounds image.Rectangle
+	drawBounds  image.Rectangle
+	tr          ebiten.GeoM
 }
 
 func (w *Window) Center() {
-	bounds := w.bounds
+	bounds := w.drawBounds
 	w.tr.Translate(float64(bounds.Min.X+bounds.Max.X)/2, float64(bounds.Min.Y+bounds.Max.Y)/2)
 }
 
 func (w *Window) Canvas(screen *ebiten.Image) Canvas {
+	if debug {
+		drawRect(screen, w.drawBounds, color.RGBA{0xff, 0, 0, 0xff})
+		drawRect(screen, w.outerBounds, color.RGBA{0, 0xff, 0, 0xff})
+	}
 	return &transformCanvas{
-		target:   screen.SubImage(w.bounds).(*ebiten.Image),
+		target:   screen.SubImage(w.drawBounds).(*ebiten.Image),
 		baseGeoM: w.tr,
 	}
 }
 
 func (w *Window) Contains(pt image.Point) bool {
-	return pt.In(w.bounds)
+	return pt.In(w.drawBounds)
 }
 
 func (w *Window) Coords(pt image.Point) (float64, float64) {
 	inv := w.tr
 	inv.Invert()
-	return inv.Apply(float64(pt.X), float64(pt.Y))
+	return inv.Apply(fc(pt))
 }
 
 func centeredWindow(bounds image.Rectangle, drawBounds image.Rectangle, tr ebiten.GeoM) *Window {
-	scaledDrawBounds := image.Rectangle{
-		Min: pt(tr.Apply(fc(drawBounds.Min))),
-		Max: pt(tr.Apply(fc(drawBounds.Max))),
+	minX, minY := tr.Apply(fc(drawBounds.Min))
+	maxX, maxY := tr.Apply(fc(drawBounds.Max))
+	bsz := bounds.Size()
+	w, h := fc(bsz)
+	dw, dh := maxX-minX, maxY-minY
+	if w < dw || h < dh {
+		scale := math.Min(w/dw, h/dh)
+		tr.Scale(scale, scale)
+		minX, minY = scale*minX, scale*minY
+		maxX, maxY = scale*maxX, scale*maxY
 	}
-	sz := scaledDrawBounds.Size()
-	diff := bounds.Size().Sub(sz).Div(2)
+	scaledDrawBounds := image.Rectangle{
+		Min: pt(minX, minY),
+		Max: pt(maxX, maxY),
+	}
+	diff := bsz.Sub(scaledDrawBounds.Size()).Div(2)
 	wbounds := image.Rectangle{Min: bounds.Min.Add(diff), Max: bounds.Max.Sub(diff)}
 	translateVec := wbounds.Min.Sub(scaledDrawBounds.Min)
 	tr.Translate(fc(translateVec))
 	return &Window{
-		bounds: wbounds,
-		tr:     tr,
+		outerBounds: bounds,
+		drawBounds:  wbounds,
+		tr:          tr,
 	}
 }
 
